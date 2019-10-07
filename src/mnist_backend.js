e3ee9041d72137e005355459394cda1ad2dcb8b7
Mohammed Hamada Gasmallah <11mhg@queensu.ca>
2019-10-07 14:02:43 -0400

    model strings for mnist are being sent through compute and received!

--- src/mnist_backend.js
+++ src/mnist_backend.js
@@ -78,20 +78,30 @@ async function computeBatch(
     return myResults;
 }
 
+async function batchify(allImages,batchSize){
+    let batchified = []
+    let curBatch = []
+    for (let i=0;i<allImages.length;i++){
+        curBatch.push(allImages[i]);
+        if (curBatch.length >= batchSize){
+            batchified.push(curBatch);
+            curBatch = [];
+        }
+    }
+    if (curBatch.length >=1){
+        batchified.push(curBatch);
+    }
+    return batchified;
+}
+
 //distributed computation of frame batches (requires an available DCP network)
 async function _callDCP(
     myImages,
     myModel,
     layersModel,
-    myCompute
+    myCompute,
+    batchSize = 4,
 ) {
-
-    let myResults = [];
-
-    //convert all pixel arrays to strings before passing to DCP
-    for (let i = 0; i < myImages.length; i++) {
-        myImages[i] = JSON.stringify(myImages[i]);
-    }
 
     let ser_model = null;
 
@@ -106,57 +116,33 @@ async function _callDCP(
     }
     myModel = null;
 
-    /*
-      //worker function to be passed to DCP, in the form of a template literal string
-      const workFunction = `async function (imageData) {
+    let inferenceObj = [];
 
-        //load necessary DCP modules
-        tf = require('tfjs');
-        nn = require('${myArchitecture}.js');
 
-        progress(0.2);
+    let myResults = [];
+    
+    myImages.push(myImages[0]);
 
-        //convert pixel string into array, remove alpha channel, set input size
-        let thisImage = JSON.parse('[' + imageData + ']');
-        thisImage = thisImage.filter((e, i) => (i + 1) % 4);
-        const imageWidth = (thisImage.length / 3) ** (1/2);
+    myImages = await batchify(myImages,batchSize);
 
-        progress(0.4);
+    for (let i = 0; i < myImages.length; i++) {
+        myImages[i] = JSON.stringify(myImages[i]);
+        inferenceObj.push({input: myImages[i], serialModel: ser_model, layersModel: layersModel});
+    }
 
-        //convert pixel array into properly configured tensor
-        //tf.setBackend('webgl');
-        let imageTensor = await tf.tidy(() => {
-          let myTensor = tf.tensor(thisImage, [imageWidth, imageWidth, 3]);
-          return myTensor.expandDims(0).toFloat().div(tf.scalar(255));
-        });
+    
 
-        progress(0.6);
+    const workFunction = `async function (inferenceObj) {
+    //load necessary DCP modules
+    tf = require('tfjs');
 
-        //load architecture
-        if (!global.hasOwnProperty('model')) {
-            global.model = await nn.default.load();
-        }
-
-        progress(0.8);
-
-        //pass pixel array tensor to model for detection 
-        let thisPrediction = await model.detectFromTensor(imageTensor);
-        imageTensor.dispose();
-
-        progress(1);
-
-        return thisPrediction;
-      }`;
-    */
-    //WORKER FUNCTION TEST AREA
-    const workFunction = `async function (imageData) {
 
     progress(1);
 
-    return imageData;
+
+    return inferenceObj;
 /*
-    //load necessary DCP modules
-    tf = require('tfjs');
+
 
     setInterval(progress(), 5000);
 
@@ -200,10 +186,12 @@ async function _callDCP(
 */
   }`;
     //define DCP generator, which receives array of image strings and a work function
-    const gen = dcp.compute.for(myImages, workFunction);
+    console.log(inferenceObj);
+
+    const gen = dcp.compute.for(inferenceObj, workFunction);
 
     //define necessary modules, which must be available on the DCP module server 
-    // gen.requires('tensorflowdcp/tfjs');
+    gen.requires('tensorflowdcp/tfjs');
 
     // gen.on('accepted', (thisOutput) => {
     //     console.log('Job accepted!');
@@ -216,8 +204,6 @@ async function _callDCP(
 
     //receives and collects the results of each worker thread
     gen.on('result', (thisOutput) => {
-        console.log('Job results:');
-        console.log(thisOutput.result);
         myResults.push(thisOutput.result);
     });
 
@@ -230,7 +216,8 @@ async function _callDCP(
         //sets gpu requirement and DCC payment, calls DCP generator on scheduler server
         //gen._generator.capabilities = {gpu: true};
         await gen.exec(0.0001);
-        console.log('all images after: ',myResults);
+        console.log('inference Object: ',myResults);
+        console.log('are the models the same?: ',inferenceObj[0].serialModel === myResults[0].serialModel);
         throw "Here";
         return myResults;
     } else if (myCompute == 'local') {
