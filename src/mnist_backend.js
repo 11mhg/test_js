81267a9fcd8821c7bc83bda52ab5643ccf993122
Hamada Gasmallah <11mhg@queensu.ca>
2019-10-08 12:50:16 -0400

    if this version works, our module is up there

--- src/mnist_backend.js
+++ src/mnist_backend.js
@@ -3,21 +3,16 @@
 import * as tf from '@tensorflow/tfjs';
 import tfserialize from './tfserialize';
 
-// async function serializeModel(layersModel){
-
-// }
-
-// async function unserializeModel(ser_model){
-
-// }
 
 //coordinates the detections on the provided batch of images
 async function computeBatch(
     myImages, myShapes, testSerialize = true, layersModel = false
 ) {
+    //compute a batch of images using the following protocol ("Useful for debugging")
     //var myCompute = 'browser';
     var myCompute = 'scheduler';
 
+    //Load the model first (Either layers or graph) useful for testing the same model but in different tfjs formats
     let myModel = null;
     if (layersModel) {
         myModel = await tf.loadLayersModel("http://127.0.0.1:5500/layers-js/model.json");
@@ -31,17 +26,18 @@ async function computeBatch(
 
         //DCP computation prompts user for a wallet, then passes on the images
         await dcp.protocol.keychain.getKeystore();
-        myResults = await _callDCP(myImages,myModel,layersModel,myCompute);
+        //call dcp with the images, the model, the boolean which asks if this is a layers model and the compute format
+        myResults = await _callDCP(myImages, myModel, layersModel, myCompute);
 
     } else if (myCompute == 'browser') {
 
-        if (testSerialize && layersModel){
+        if (testSerialize && layersModel) {
             console.log("Layers Model is being Serialized");
-            let ser_model = await tfserialize.serialize(myModel,false);
+            let ser_model = await tfserialize.serialize(myModel, false);
             console.log("Model size after serialization is: ", Buffer.byteLength(ser_model, 'utf8'));
             myModel = await tfserialize.deserialize(ser_model);
             console.log("Model has been deserialized and replaced");
-        }else if (testSerialize && !layersModel){
+        } else if (testSerialize && !layersModel) {
             console.log("Graph Model is being Serialized");
             let ser_model = await tfserialize.serializeGraph(myModel);
             console.log("Model size after serialization is: ", Buffer.byteLength(ser_model, 'utf8'));
@@ -78,17 +74,19 @@ async function computeBatch(
     return myResults;
 }
 
-async function batchify(allImages,batchSize){
+//function takes an array of [num_images] and turns it into [[batch_size]*(min(1,num_images//batch_size))] 
+// if there are less than batchSize images, we simply return [[num_images]]
+async function batchify(allImages, batchSize) {
     let batchified = []
     let curBatch = []
-    for (let i=0;i<allImages.length;i++){
+    for (let i = 0; i < allImages.length; i++) {
         curBatch.push(allImages[i]);
-        if (curBatch.length >= batchSize){
+        if (curBatch.length >= batchSize) {
             batchified.push(curBatch);
             curBatch = [];
         }
     }
-    if (curBatch.length >=1){
+    if (curBatch.length >= 1) {
         batchified.push(curBatch);
     }
     return batchified;
@@ -103,94 +101,63 @@ async function _callDCP(
     batchSize = 4,
 ) {
 
+    //begin by serializing model
     let ser_model = null;
 
-    if (layersModel){
+    if (layersModel) {
         console.log("Layers Model is being Serialized");
-        ser_model = await tfserialize.serialize(myModel,false);
+        ser_model = await tfserialize.serialize(myModel, false);
         console.log("Model size after serialization is: ", Buffer.byteLength(ser_model, 'utf8'));
-    }else if (!layersModel){
+    } else if (!layersModel) {
         console.log("Graph Model is being Serialized");
         ser_model = await tfserialize.serializeGraph(myModel);
         console.log("Model size after serialization is: ", Buffer.byteLength(ser_model, 'utf8'));
     }
+    //set myModel to null for gc
     myModel = null;
 
 
     let myResults = [];
-    
+
+    //(here I am just testing out some batch stuff so I push the same images on a second time.)
     myImages.push(myImages[0]);
 
-    myImages = await batchify(myImages,batchSize);
+    //batchify the images
+    myImages = await batchify(myImages, batchSize);
 
+
+    //stringify the image batches
     for (let i = 0; i < myImages.length; i++) {
         myImages[i] = JSON.stringify(myImages[i]);
     }
 
-    let inferenceObj = {serialModel: ser_model, layersModel: layersModel};
+    //the inference Object contains all we need for inference (in the case of mnist)
+    let inferenceObj = { serialModel: ser_model, layersModel: layersModel };
 
-    
 
+    //the actual work function being run on the other side
     const workFunction = `async function (images, inferenceObj) {
     //load necessary DCP modules
     tf = require('tfjs');
+    serialize = require('serialize');
+    tfserialize = require('tfserialize');
 
 
     progress(1);
 
 
     return inferenceObj;
-/*
-
-
-    setInterval(progress(), 5000);
-
-    progress(0.2);
-
-    //convert pixel string into array, remove alpha channel, set input size
-    let thisImage = JSON.parse('[' + imageData + ']');
-    thisImage = thisImage.filter((e, i) => (i + 1) % 4);
-    const imageWidth = (thisImage.length / 3) ** (1/2);
-
-    //convert modelData to model;
-
-    progress(0.4);
-
-    //convert pixel array into properly configured tensor
-    //tf.setBackend('webgl');
-    let imageTensor = await tf.tidy(() => {
-      let myTensor = tf.tensor(thisImage, [imageWidth, imageWidth, 3]);
-      return myTensor.expandDims(0).toFloat().div(tf.scalar(255));
-    });
-
-    progress(0.6);
-
-    //load architecture
-    let model = await nn.load('https://aisight.ca/prestriate/model/model.json');
-
-//    if (!global.hasOwnProperty('model')) {
-//      global.model = await nn.default.load();
-//    }
-
-    progress(0.8);
-
-    //pass pixel array tensor to model for detection 
-    //let thisPrediction = await model.detectFromTensor(imageTensor);
-    //imageTensor.dispose();
-
-    progress(1);
-
-    //return thisPrediction;
-    return imageTensor;
-*/
+        
   }`;
-    //define DCP generator, which receives array of image strings and a work function
-    console.log(inferenceObj);
 
-    const gen = dcp.compute.for(myImages, workFunction,[inferenceObj]);
+    //the generator for dcp compute protocol: myImages will be sliced out and sent to different workers, 
+    //while inferenceObj will be sent to each of the workers in it's entirety.
+    const gen = dcp.compute.for(myImages, workFunction, [inferenceObj]);
 
     //define necessary modules, which must be available on the DCP module server 
+    gen.requires('dcp-core/serialize')
     gen.requires('tensorflowdcp/tfjs');
+    gen.requires('modelserial/tfserialize.js');
 
     // gen.on('accepted', (thisOutput) => {
     //     console.log('Job accepted!');
@@ -214,9 +181,13 @@ async function _callDCP(
     if (myCompute == 'scheduler') {
         //sets gpu requirement and DCC payment, calls DCP generator on scheduler server
         //gen._generator.capabilities = {gpu: true};
+        //this is the actual call, the 0.0001 is the amount we are willing to pay per slice
         await gen.exec(0.0001);
-        console.log('inference Object: ',myResults);
-        console.log('are the models the same?: ',inferenceObj.serialModel === myResults[0].serialModel);
+
+        // the results
+        console.log('inference Object: ', myResults);
+        //check if string model is the same as the one before we sent (making sure nothing is being corrupted)
+        console.log('are the models the same?: ', inferenceObj.serialModel === myResults[0].serialModel);
         throw "Here";
         return myResults;
     } else if (myCompute == 'local') {
